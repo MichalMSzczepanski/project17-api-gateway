588e0ab800ec25a55c8d01ed69f5721b5e074bdf
michals <michal.m.szczepanski@gmail.com>
2024-08-27 14:02:29 +0200

    semantics: remove unused imports

--- src/main/java/work/szczepanskimichal/exception/GlobalExceptionHandler.java
+++ src/main/java/work/szczepanskimichal/exception/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import work.szczepanskimichal.configuration.ServiceAddressConfiguration;
 
 import java.net.ConnectException;
 
